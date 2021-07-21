96b066fea8048a00d7346c398b9ec85c257b6771
earl562 <earl1.perry@famu.edu>
2021-07-21 16:11:58 -0400

    Set & random number generator

---
+++ src/Components/money.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export default class money extends Component {
+        constructor (props) {
+            super(props)
+            this.state = {
+                allowance: Math.random()*10
+            }
+        this.updateMoney = this.updateMoney.bind(this)
+        this.setMoney = this.setMoney.bind(this)
+        this.randomMoney = this.randomMoney.bind(this)
+        }
+
+updateMoney() {
+    this.setState ({allowance: this.state.allowance-1})
+
+}
+
+setMoney() {
+    this.setState ({allowance: 10})
+
+}
+
+randomMoney() {
+    this.setState ({allowance: Math.random()*10})
+
+}
+
+
+
+
+    render() {
+        return (
+            <div>
+                You have ${this.state.allowance.toFixed(2)}
+                <button onClick ={this.setMoney} > Set amount  </button>
+                < button onClick ={this.randomMoney} > Random  </button>
+            </div>
+        )
+    }
+}
+
+
+
+
