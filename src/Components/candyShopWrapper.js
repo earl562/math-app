17ea64e54743a352e90a93f883e488cbd226ddf0
dcostag <87328463+dcostag@users.noreply.github.com>
2021-07-22 13:28:07 -0400

    uploaded icons

--- src/Components/candyShopWrapper.js
+++ src/Components/candyShopWrapper.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import CandyItemObject from './candyItemObject'
 import CandyItem from './candyItem'
+import ChocolateBar from '../images/chocolate-bar.png'
+import CottonCandy from '../images/cotton-candy.png'
+import Taffy from '../images/taffy.png'
+import GummyBear from '../images/gummy-bear.png'
+import Candy from '../images/candy.png'
+import Lollipops from '../images/lollipops.png'
+
 
 
 
 export default function CandyShopWrapper(props) {
-    let snickers = new CandyItemObject('Snickers',1.5,'https://toppng.com/uploads/preview/snickers-png-11552942897mvei4yipwi.png')
-    let twizzlers = new CandyItemObject('Twizzlers',1,"https://i5.walmartimages.com/asr/be06ff09-5144-4510-8ac6-172d9d0309e9.31b2d286d7704c133d8f0a847cfe161a.jpeg")
+    let chocolateBar = new CandyItemObject('Chocolate Bar', 1.5, ChocolateBar)
+    let cottonCandy = new CandyItemObject('Cotton Candy', 1.5, CottonCandy)
+    let gummyBear = new CandyItemObject('Gummy Bear', 1.5, GummyBear)
+    let lollipops = new CandyItemObject('Lollipops', 1.5, Lollipops)
+    let candy = new CandyItemObject('Candy', 1.5, Candy)
+    let taffy = new CandyItemObject('Taffy', 1.5, Taffy)
     
-    
-    let candies = [snickers,twizzlers,twizzlers,twizzlers,twizzlers,twizzlers]
+    let candies = [chocolateBar,cottonCandy,gummyBear,lollipops,candy,taffy]
     
     
     
