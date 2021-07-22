3511f4968f98b305febe61259518af83c6a08232
Andrew Wolstenholme <ajwsail99@gmail.com>
2021-07-22 09:59:55 -0400

    some styling

--- src/App.js
+++ src/App.js
@@ -6,30 +6,28 @@ import { useState } from 'react';
 
 function App() {
   
-  const [allowance, setAllowance] = useState((Math.random()*10).toFixed(2));
-  const updateMoney = (price) => { 
-    setAllowance(allowance - price);
-  }
+    const [allowance, setAllowance] = useState((Math.random()*10).toFixed(2));
+    const updateMoney = (price) => { 
+        setAllowance(allowance - price);
+    }
 
-  return (
-    <div className="App">
-      {/*Header Component */}
-      <p>{allowance}</p>
-	  <input id={"allowanceInput"}/>
-	  <button onClick={() => setAllowance(document.getElementById("allowanceInput").value)}>
-		  Set Money
-	    </button>
-	  <button onClick={() => setAllowance((Math.random() *10 ).toFixed(2))}>
-          Random Amount
-        </button>
-      <CandyShopWrapper 
-        updateMoney={updateMoney}
-      />
-	  {/*Money view Component*/}
-	  {/*Footer Component*/}      
+    return (
+        <div className="App">
+            {/*Header Component */}
+            <p>{allowance}</p>
+            <input id={"allowanceInput"}/>
+            <button onClick={() => setAllowance(document.getElementById("allowanceInput").value)}>
+                Set Money
+            </button>
+            <button onClick={() => setAllowance((Math.random() *10 ).toFixed(2))}>
+                Random Amount
+            </button>
+            <CandyShopWrapper updateMoney={updateMoney}/>
+            {/*Money view Component*/}
+            {/*Footer Component*/}      
 
-    </div>
-  );
+        </div>
+    );
 }
 
 export default App;
