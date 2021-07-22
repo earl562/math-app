f617bdb0dd375f4365b786ccd195dea0ec3f58a1
lithelms <hoenzsch.william@gmail.com>
2021-07-22 14:39:27 -0400

    awning add

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Money from './Components/money.js'
 import Header from './Components/header'
 import Footer from './Components/footer'
+import Awning from './Components/awning'
 
 
 function App() {
     return (
         <div className="App">
-            <Header />    
+            <Header />
+            <Awning />    
             <Money />
             <Footer />
         </div>
