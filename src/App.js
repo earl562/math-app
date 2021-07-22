6dca3e73fd642c9230c12227fb75d5dab892a2c2
earl562 <earl1.perry@famu.edu>
2021-07-22 10:53:57 -0400

    Money view key tag

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React from 'react'
 import logo from './logo.svg';
 import './App.css';
 import CandyShopWrapper from './Components/candyShopWrapper';
+import Money from './Components/money.js'
 import { useState } from 'react';
+
 
 function App() {
   
-    const [allowance, setAllowance] = useState((Math.random()*10).toFixed(2));
-    const updateMoney = (price) => { 
-        setAllowance(allowance - price);
-    }
+    //const [allowance, setAllowance] = useState((Math.random()*10).toFixed(2));
+    //const updateMoney = (price) => { 
+        //setAllowance(allowance - price);
+    
 
     return (
         <div className="App">
             {/*Header Component */}
-            <p>{allowance}</p>
+            {/* <p>{allowance}</p>
             <input id={"allowanceInput"}/>
             <button onClick={() => setAllowance(document.getElementById("allowanceInput").value)}>
                 Set Money
@@ -22,10 +24,10 @@ function App() {
             <button onClick={() => setAllowance((Math.random() *10 ).toFixed(2))}>
                 Random Amount
             </button>
-            <CandyShopWrapper updateMoney={updateMoney}/>
+            <CandyShopWrapper updateMoney={updateMoney}/> */}
             {/*Money view Component*/}
             {/*Footer Component*/}      
-
+        <Money />
         </div>
     );
 }
