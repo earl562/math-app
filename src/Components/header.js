2dde8765c61f3fa7ead1aaa25e8f677eef44ba1c
earl562 <earl1.perry@famu.edu>
2021-07-23 14:37:04 -0400

    Changetheme function

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,13 +2,20 @@ import React from 'react'
 import Logo from '../images/logo.png'
 
 export default function header() {
+   function changeTheme(){
+    var elements = document.getElementsByClassName('candyBox'); 
+    for(var i = 0; i < elements.length; i++)
+     {		elements[i].style.backgroundColor = "#F2C4E6" ;	}
+
+   } 
+    
     return (
 
         <div class="firstDiv">
             <div class="right-col">
                 
-                <button className="submit2">
-                        Set Amount
+                <button className="submit2" onClick="changeTheme">
+                         change theme
                 </button>
             </div>
             <img className="logoTop" alt="logo" src={Logo}/>
